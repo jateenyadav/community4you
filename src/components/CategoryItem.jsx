1e642cb55219f346287434d496a492db11bfa26f
Jateen Yadav <jateen.yadav@hitachi.co.in>
2025-08-12 10:20:45 +0530

    changes

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -50,7 +50,7 @@ const CategoryItem = ({ item }) => {
       <Image src={item.img} />
       <Info>
         <Title>{item.title}</Title>
-        <Button>About</Button>
+        <Button>Abouts</Button>
       </Info>
     </Container>
   );
